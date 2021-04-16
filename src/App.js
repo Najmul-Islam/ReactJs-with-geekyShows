1e292004fda7c89a61eecc7899014f70ff32f1eb
Najmul-Islam <najmulislam519@gmail.com>
2021-04-17 00:51:04 +0600

    complete updating stage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Student from './Student';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Student />
+            </div>
+        );
+    }
+}
+
+export default App;

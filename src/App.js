72c33eb63e147755bbe74912408d85381cb50915
Najmul-Islam <najmulislam519@gmail.com>
2021-04-18 00:51:03 +0600

    leared useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, {useState} from 'react'
 
 function App (){
-    const nameSteteVariable = useState("Rahul");
+    const [name, setName] = useState("Rahul");
+    const [roll, setRoll] = useState(101)
+    // const name = nameSteteVariable[0]
+    // const setName = nameSteteVariable[1]
+
+    const handleClick = () =>{
+        setName('GeekyShows');
+        setRoll(102)
+    }
 
     return(<div>
-        <h1>{nameSteteVariable[0]}</h1>
+        <h1>{name}</h1>
+        <h2>{roll}</h2>
+        <button type="button" onClick={handleClick}>Change </button>
     </div>)
 }
 
